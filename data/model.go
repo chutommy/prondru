1d85c836fc37a8e08e87f1413281d659eca34b04
chutified <tommychu2256@gmail.com>
2021-01-19 12:43:47 +0100

    Add json tags and export Request struct

--- data/model.go
+++ data/model.go
@@ -12,15 +12,15 @@ type Request struct {
 
 // Response represents a search result.
 type Response struct {
-	id                 string
-	title              string
-	authors            []string
-	subjects           []string
-	description        string
-	doi                string
-	publisher          string
-	countryPublication string
-	publicationDate    string
+	Id                 string   `json:"osti_id"`
+	Title              string   `json:"title"`
+	Authors            []string `json:"authors"`
+	Subjects           []string `json:"subjects"`
+	Description        string   `json:"description"`
+	Doi                string   `json:"doi"`
+	Publisher          string   `json:"publisher"`
+	CountryPublication string   `json:"country_publication"`
+	PublicationDate    string   `json:"publication_date"`
 }
 
 // Results represents a list of Response.
