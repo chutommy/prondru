c994d45cc245ae64a95d0c40fa3e2fa646204217
chutified <tommychu2256@gmail.com>
2021-01-19 11:48:42 +0100

    Implement data models

--- data/model.go
+++ data/model.go
@@ -1,1 +1,27 @@
 package data
+
+// Request represents a search query configuration.
+type Request struct {
+	id     string
+	query  string
+	author string
+	title  string
+	rows   int
+	page   int
+}
+
+// Response represents a search result.
+type Response struct {
+	id                 string
+	title              string
+	authors            []string
+	subjects           []string
+	description        string
+	doi                string
+	publisher          string
+	countryPublication string
+	publicationDate    string
+}
+
+// Results represents a list of Response.
+type Results []*Response
