73a42593336c72285f4f9a04742aec7e3dd2c5ec
chutified <tommychu2256@gmail.com>
2021-01-19 15:41:41 +0100

    Implement selecting functionalities

---
+++ handler/select.go
@@ -0,0 +1,26 @@
+package handler
+
+import (
+	"fmt"
+
+	"prondru/data"
+
+	"github.com/manifoldco/promptui"
+)
+
+// selectRecord asks user to choose one of the articles.
+func selectRecord(records data.Responses) (*data.Response, error) {
+	selection := promptui.Select{
+		Label: "Select article",
+		Items: records.ToStrings(),
+		Size:  10,
+	}
+
+	// run the selection
+	i, _, err := selection.Run()
+	if err != nil {
+		return nil, fmt.Errorf("failed to rertieve a selection: %w", err)
+	}
+
+	return records[i], nil
+}
