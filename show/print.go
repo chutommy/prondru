93f5aa186c135fdd908481840a8cd11f8a352a4e
chutified <tommychu2256@gmail.com>
2021-01-19 17:45:23 +0100

    Implement ui printer

---
+++ show/print.go
@@ -0,0 +1,60 @@
+package show
+
+import (
+	"fmt"
+	"strings"
+
+	"prondru/data"
+
+	"github.com/fatih/color"
+)
+
+// PrintResponse prints formatted article.
+//goland:noinspection ALL
+func PrintResponse(r *data.Response) {
+	b := color.New(color.Bold)
+	u := color.New(color.Underline)
+	i := color.New(color.Italic)
+	f := color.New(color.Faint)
+
+	fmt.Printf("\n")
+	fmt.Printf("================\n")
+
+	// {title} [OSTI: {id}]
+	b.Printf("%s", r.Title)
+	fmt.Printf(" ")
+	u.Printf("[OSTI: %s]", r.ID)
+	fmt.Printf("\n")
+
+	// Authors: {authors}
+	// Publisher: {publisher} - {country}
+	b.Printf("Authors: ")
+	i.Printf("%s\n", join(r.Authors))
+	b.Printf("Publisher: ")
+	fmt.Printf("%s - %s\n", r.Publisher, r.CountryPublication)
+
+	// Subjects: {subjects}
+	b.Printf("Subjects: ")
+	fmt.Printf("%s\n", join(r.Subjects))
+
+	// Date: {publication data} ({url})
+	b.Printf("Date: ")
+	f.Printf("%s (%s)\n", r.PublicationDate, r.Doi)
+
+	fmt.Printf("----------------\n")
+
+	// Research
+	// {description}
+	u.Printf("Research\n")
+	fmt.Printf("%s\n", r.Description)
+	fmt.Printf("\n")
+}
+
+// join joins slice of strings.
+func join(ss []string) string {
+	if len(ss) >= 7 {
+		ss = ss[:7]
+	}
+
+	return strings.Join(ss, ", ")
+}
