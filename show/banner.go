0b2900a0ed343d18c58290ef5d27b842643bbacd
chutified <tommychu2256@gmail.com>
2021-01-20 11:26:51 +0100

    Update banner, binaries

--- show/banner.go
+++ show/banner.go
@@ -11,26 +11,26 @@ func Banner() string {
 
 // bannerString defines the banner string.
 const bannerString = `
-------------------------------------------------------------
-██████╗ ██████╗  ██████╗ ███╗   ██╗██████╗ ██████╗ ██╗   ██╗
-██╔══██╗██╔══██╗██╔═══██╗████╗  ██║██╔══██╗██╔══██╗██║   ██║
-██████╔╝██████╔╝██║   ██║██╔██╗ ██║██║  ██║██████╔╝██║   ██║
-██╔═══╝ ██╔══██╗██║   ██║██║╚██╗██║██║  ██║██╔══██╗██║   ██║
-██║     ██║  ██║╚██████╔╝██║ ╚████║██████╔╝██║  ██║╚██████╔╝
-╚═╝     ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═════╝ ╚═╝  ╚═╝ ╚═════╝
-------------------------------------------------------------
- CLI for Office of Scientific and Technical Information API
-             https://www.osti.gov/api/v1/docs
-------------------------------------------------------------
-               Welcome to the prOndru v18.0!
-                          ---
-        Ondra's terminal gateway to the fantastic
-             world of physical discoveries.
-               ~ Wednesday, 20 Jan 2021
-
-       Copyright (c) 2021 Tommy Chu & Ondrej Skrna
-             Licensed under the MIT license
-------------------------------------------------------------
+--------------------------------------------------------------
+  ██████╗ ██████╗  ██████╗ ███╗   ██╗██████╗ ██████╗ ██╗   ██╗
+  ██╔══██╗██╔══██╗██╔═══██╗████╗  ██║██╔══██╗██╔══██╗██║   ██║
+  ██████╔╝██████╔╝██║   ██║██╔██╗ ██║██║  ██║██████╔╝██║   ██║
+  ██╔═══╝ ██╔══██╗██║   ██║██║╚██╗██║██║  ██║██╔══██╗██║   ██║
+  ██║     ██║  ██║╚██████╔╝██║ ╚████║██████╔╝██║  ██║╚██████╔╝
+  ╚═╝     ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═════╝ ╚═╝  ╚═╝ ╚═════╝
+--------------------------------------------------------------
+   CLI for Office of Scientific and Technical Information API
+               https://www.osti.gov/api/v1/docs
+--------------------------------------------------------------
+                 Welcome to the prOndru v18.0!
+                            ---
+          Ondra's terminal gateway to the fantastic
+               world of physical discoveries.
+                 ~ Wednesday, 20 Jan 2021
+  
+         Copyright (c) 2021 Tommy Chu & Ondrej Skrna
+               Licensed under the MIT license
+--------------------------------------------------------------
 Discover over 70 years of research results
 from 3 million scientific records
 provided by OSTI.
