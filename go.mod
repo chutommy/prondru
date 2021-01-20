aa70150675f48f7e8beaab14384a4c57e897849c
chutified <tommychu2256@gmail.com>
2021-01-20 10:41:12 +0100

    Implement banner

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module prondru
 go 1.15
 
 require (
+	github.com/dimiro1/banner v1.1.0
 	github.com/fatih/color v1.10.0
 	github.com/manifoldco/promptui v0.8.0
+	github.com/mattn/go-colorable v0.1.8
 	github.com/urfave/cli/v2 v2.3.0
 )
--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"log"
 	"os"
 	"prondru/controller"
+
+	"github.com/dimiro1/banner"
+	_ "github.com/dimiro1/banner/autoload"
+	"github.com/mattn/go-colorable"
 )
 
 func main() {
+	// print banner
+	banner.Init(colorable.NewColorableStdout(), true, false, bytes.NewBufferString(""))
+	fmt.Println()
+
+	// run CLI
 	app := controller.NewApp()
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
